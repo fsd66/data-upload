ad5106af6c8db6999b5c3ae2e41024410d13fd74
fsd66 <fsd66@rocketmail.com>
2020-10-28 03:11:23 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+
+const app = express();
+const port = process.env.PORT;
+const saveLocation = process.env.SAVE_LOCATION || __dirname;
+console.log(`save location: ${saveLocation}`);
+
+app.use(fileUpload());
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/upload", (req, res) => {
+    res.redirect("/");
+})
+
+app.post("/upload", (req, res) => {
+    try {
+        const file = req.files.file;
+        console.log(`Received file: ${file.name}, Size: ${file.size}`);
+        const filePath = path.join(saveLocation, file.name);
+        console.log(`Writing file to: ${filePath}`);
+        fs.writeFile(filePath, file.data, (err) => console.log(err ?? `File successfully written!`));
+        res.send("File received.");
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(415);
+    }
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
